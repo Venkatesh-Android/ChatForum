a5edc36d60e4e9ff7f418972da4ce86b0d61ce89
Venkatesh-Android <venkatesh@venkateshs-MacBook-Pro.local>
2016-09-20 23:42:21 +0530

    ChatForum
    Little worked and had Fun

---
+++ src/main/java/com/ChatForum/ProfileRole/ProfileRoleDAO.java
@@ -0,0 +1,12 @@
+package com.ChatForum.ProfileRole;
+import java.util.List;
+
+public interface ProfileRoleDAO
+{
+	public void insertProfileRole(ProfileRole i);
+	public void deleteProfileRole(int i);
+	public void updateProfileRole(ProfileRole i);
+	public void generateProfileRoles();
+	public ProfileRole getProfileRole(int i);
+    public List<ProfileRole> getAllProfileRoles();
+}
